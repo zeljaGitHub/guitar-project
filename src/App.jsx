60072136c92d7d6d6a9c9eaf56948928bbdbf4c6
Zeljko <zel87king2@gmail.com>
2023-04-13 18:56:15 +0200

    A lot of new features

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { notes } from "./components/notes";
+import Guitar from "react-guitar";
 import Soundfont from "soundfont-player";
 import "./App.css";
 
@@ -9,6 +11,10 @@ const GuitarScale = () => {
   const [isPlaying, setIsPlaying] = useState(false);
   const [lastMelody, setLastMelody] = useState([]);
   const [numNotes, setNumNotes] = useState(8);
+  const [bpm, setBpm] = useState(120);
+  const [timeouts, setTimeouts] = useState([]);
+  const [selectedNotesArray, setSelectedNotesArray] = useState("cMajor");
+  const [pickInstrument, setPickInstrument] = useState("acoustic_guitar_steel");
 
   const handleNumNotesChange = (event) => {
     const value = event.target.value;
@@ -17,120 +23,138 @@ const GuitarScale = () => {
     }
   };
 
+  const handleBpmChange = (event) => {
+    const value = event.target.value;
+    if (value >= 60 && value <= 240) {
+      setBpm(value);
+    }
+  };
+
+  const handleInstrumentChange = (event) => {
+    const newInstrument = event.target.value;
+    setPickInstrument(newInstrument);
+  };
+
+  const stopMelody = () => {
+    timeouts.forEach((timeoutId) => clearTimeout(timeoutId));
+    setNotesPlayed([]);
+    // setLastMelody([]);
+    setCurrentNote("");
+    setIsPlaying(false);
+    setTimeouts([]);
+  };
+
   useEffect(() => {
-    Soundfont.instrument(new AudioContext(), "acoustic_guitar_steel").then(
-      (player) => {
-        setPiano(player);
-      }
-    );
-  }, []);
-
-  const playCmajorScale = () => {
-    setIsPlaying(true);
-    const notes = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"];
-
-    notes.forEach((note, index) => {
-      setTimeout(() => {
-        setCurrentNote(note);
-        piano.play(note);
-        if (index === notes.length - 1) {
-          setTimeout(() => {
-            setCurrentNote("");
-            setIsPlaying(false);
-          }, 500);
-        }
-      }, index * 500);
+    Soundfont.instrument(new AudioContext(), pickInstrument).then((player) => {
+      setPiano(player);
     });
-  };
+    return () => {
+      timeouts.forEach((timeoutId) => clearTimeout(timeoutId));
+    };
+  }, [pickInstrument]);
 
-  const playReverseCmajorScale = () => {
+  const playRandomMelody = (numNotes, bpm, notesArray) => {
     setIsPlaying(true);
-    const notes = ["C5", "B4", "A4", "G4", "F4", "E4", "D4", "C4"];
-
-    notes.forEach((note, index) => {
-      setTimeout(() => {
-        setCurrentNote(note);
-        piano.play(note);
-        if (index === notes.length - 1) {
-          setTimeout(() => {
-            setCurrentNote("");
-            setIsPlaying(false);
-          }, 500);
-        }
-      }, index * 500);
-    });
-  };
-
-  const playRandomMelody = (numNotes) => {
-    setIsPlaying(true);
-    const notes = ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"];
     const melody = [];
+    const newTimeouts = [];
 
     for (let i = 0; i < numNotes; i++) {
-      const randomNoteIndex = Math.floor(Math.random() * notes.length);
-      melody.push(notes[randomNoteIndex]);
+      const randomNoteIndex = Math.floor(Math.random() * notesArray.length);
+      melody.push(notesArray[randomNoteIndex]);
     }
 
     setNotesPlayed([]);
     setLastMelody(melody);
-    melody.forEach((note, index) => {
-      setTimeout(() => {
-        if (note) {
-          setCurrentNote(note);
-          setNotesPlayed((prevNotesPlayed) => [...prevNotesPlayed, note]);
-          piano.play(note);
-          if (index === melody.length - 1) {
-            setTimeout(() => {
-              setCurrentNote("");
-              setIsPlaying(false);
-            }, melody.length * 100);
-          }
-        }
-      }, index * 500);
-    });
+
+    const millisecondsPerBeat = 60000 / bpm;
+    let timeoutId;
+    let currentIndex = 0;
+
+    const playNote = () => {
+      const note = melody[currentIndex];
+      if (note) {
+        setCurrentNote(note);
+        setNotesPlayed((prevNotesPlayed) => [...prevNotesPlayed, note]);
+        piano.play(note);
+        currentIndex++;
+        timeoutId = setTimeout(playNote, millisecondsPerBeat);
+        newTimeouts.push(timeoutId);
+      } else {
+        setCurrentNote("");
+        setIsPlaying(false);
+      }
+    };
+
+    playNote();
+    newTimeouts.push(timeoutId);
+
+    setTimeouts(newTimeouts);
   };
 
   const replayLastMelody = () => {
     if (lastMelody.length > 0) {
       setIsPlaying(true);
       setNotesPlayed([]);
-      lastMelody.forEach((note, index) => {
-        setTimeout(() => {
-          if (note) {
-            setCurrentNote(note);
-            setNotesPlayed((prevNotesPlayed) => [...prevNotesPlayed, note]);
-            piano.play(note);
-            if (index === lastMelody.length - 1) {
-              setTimeout(() => {
-                setCurrentNote("");
-                setIsPlaying(false);
-              }, lastMelody.length * 100);
-            }
-          }
-        }, index * 500);
-      });
+      const millisecondsPerBeat = 60000 / bpm;
+      let timeoutId;
+      let currentIndex = 0;
+      const newTimeouts = [];
+
+      const playNote = () => {
+        const note = lastMelody[currentIndex];
+        if (note) {
+          setCurrentNote(note);
+          setNotesPlayed((prevNotesPlayed) => [...prevNotesPlayed, note]);
+          piano.play(note);
+          currentIndex++;
+          timeoutId = setTimeout(playNote, millisecondsPerBeat);
+          newTimeouts.push(timeoutId);
+        } else {
+          setCurrentNote("");
+          setIsPlaying(false);
+        }
+      };
+
+      playNote();
+      newTimeouts.push(timeoutId);
+
+      setTimeouts(newTimeouts);
     }
   };
 
   return (
     <div>
-      <h1>Play C Major Scale on Guitar</h1>
-      <button onClick={playCmajorScale} disabled={isPlaying}>
-        Play C Major Scale
-      </button>
-      <button onClick={playReverseCmajorScale} disabled={isPlaying}>
-        Play Reverse C Major Scale
-      </button>
+      <select value={pickInstrument} onChange={handleInstrumentChange}>
+        <option value="acoustic_guitar_steel">Acoustic Guitar</option>
+        <option value="acoustic_grand_piano">Grand Piano</option>
+        <option value="taiko_drum">Drums</option>
+      </select>
+      <select
+        value={selectedNotesArray}
+        onChange={(event) => setSelectedNotesArray(event.target.value)}
+      >
+        <option value="cMajor">C Major</option>
+        <option value="aMinor">A Minor</option>
+        {/* add more options as needed */}
+      </select>
       <input type="number" value={numNotes} onChange={handleNumNotesChange} />
-      <button onClick={() => playRandomMelody(numNotes)} disabled={isPlaying}>
-        Play Random Melody ({numNotes} Notes)
+      <input type="number" value={bpm} onChange={handleBpmChange} />
+      <button
+        onClick={() =>
+          playRandomMelody(numNotes, bpm, notes[selectedNotesArray])
+        }
+        disabled={isPlaying}
+      >
+        Play Random Melody ({numNotes} Notes) at {bpm} BPM
       </button>
-
       <button
         onClick={replayLastMelody}
         disabled={isPlaying || !lastMelody.length}
       >
         Replay Last Melody
+      </button>
+      <button disabled={!isPlaying} onClick={stopMelody}>
+        Stop
       </button>
       <p>Current Note: {currentNote}</p>
       <div style={{ marginTop: "1rem" }}>
@@ -142,6 +166,27 @@ const GuitarScale = () => {
             <br />
           </>
         )}
+      </div>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Guitar
+          tuning={["E4", "B3", "G3", "D3", "A2", "E2"]}
+          frets={24}
+          fretWidth={50}
+          fretHeight={80}
+          strings={[
+            { note: "E2", color: "white" },
+            { note: "A2", color: "white" },
+            { note: "D3", color: "white" },
+            { note: "G3", color: "white" },
+            { note: "B3", color: "white" },
+            { note: "E4", color: "white" },
+          ]}
+          markers={[3, 5, 7, 9, 12, 15, 17, 19, 21]}
+          activeNotes={notesPlayed}
+          activeColor="red"
+          width={700}
+          height={300}
+        />
       </div>
     </div>
   );
---
+++ src/components/notes.jsx
@@ -0,0 +1,5 @@
+export const notes = {
+  cMajor: ["C4", "D4", "E4", "F4", "G4", "A4", "B4", "C5"],
+  aMinor: ["A4", "B4", "C5", "D5", "E5", "F5", "G5", "A5"],
+  // add more arrays as needed
+};
